62c12994e385838a4effea1cd10531de60b28253
thallysbraz <thallysbraz3@gmail.com>
2020-02-18 09:46:09 -0300

    Validações

--- services/PlansService.js
+++ services/PlansService.js
@@ -1,8 +1,12 @@
-var Plan = require("../models/plan"); //Model de Planos
+var Database = require("../models/index"); //Model de Planos
 
 class PlansServices {
+  constructor() {
+    this.Plan = Database["Plan"];
+  }
+
   async store(plans) {
-    var errors = [];
+    var errors = {};
 
     if (plans.import != undefined) {
       plans.import = true;
@@ -10,14 +14,50 @@ class PlansServices {
       plans.import = false;
     }
 
-    try {
-      await Plan.create(plans);
+    var isValid = await this.validate(plans, errors);
 
-      return true;
-    } catch (err) {
-      errors.push("Não foi possivel salvar o plano!");
+    if (isValid) {
+      try {
+        await this.Plan.create(plans);
 
+        return true;
+      } catch (err) {
+        errors.system_msg = "Não foi possivel salvar o plano!";
+
+        return errors;
+      }
+    } else {
       return errors;
+    }
+  }
+
+  validate(plan, errors) {
+    var erroCount = 0;
+
+    if (plan.title == undefined) {
+      errors.title_msg = "o título é inválido";
+      erroCount++;
+    } else {
+      if (plan.title.length < 3) {
+        errors.title_msg = "o título é inválido";
+        erroCount++;
+      }
+    }
+
+    if (plan.list == undefined) {
+      errors.list_msg = "A quantidade de listas e inválida";
+      erroCount++;
+    } else {
+      if (plan.list < 1) {
+        errors.list_msg = "A quantidade de listas e inválida";
+        erroCount++;
+      }
+    }
+
+    if (erroCount == 0) {
+      return true;
+    } else {
+      return false;
     }
   }
 }

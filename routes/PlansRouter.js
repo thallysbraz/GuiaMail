972e3577a76d7b6b3c00e2231b4d2f3a05517829
thallysbraz <thallysbraz3@gmail.com>
2020-02-26 11:48:32 -0300

    view de editar

--- routes/PlansRouter.js
+++ routes/PlansRouter.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var PlansController = require("../controllers/PlansController"); //Controler de planos
 
-router.get("/plans", PlansController.index);
+router.get("/admin/plans", PlansController.index); //Rota para listar todos os planos
 
 router.get("/admin/plans/create", PlansController.create); //Render view de add plano
 router.post("/plans/store", PlansController.store); //Rota pra salvar dados de criação
